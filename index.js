6c82b705c1478e624d9f06f16782758872b4861c
JayBlaq10 <mochhejeison@gmail.com>
2019-09-18 14:36:09 +0100

    'dropbox'

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("navbar").style.background = " white";
+   
+  } else {
+    document.getElementById("navbar").style.background = "";
+    
+  }
+}
